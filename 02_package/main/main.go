10473a10d261673f6b140ee3a99cec20eced07cf
vasilegroza <vasilegroza3@gmail.com>
2018-06-01 19:08:25 +0300

    Add first package

---
+++ 02_package/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"vasilegroza/02_package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!og olleH"))
+	fmt.Println(stringutil.MyName)
+}
---
+++ 02_package/stringutil/reverse.go
@@ -0,0 +1,9 @@
+// package stringutil contains utility functions for working with strings
+package stringutil
+
+// package scope
+// the functions witch have lowercase name start won't be visible outside the package
+
+func Reverse(s string) string {
+	return reverse_two(s)
+}
